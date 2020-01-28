a6019c519886f0ffe2e97302fbf168dfbecf392a
kadirumac <kadirumac49@gmail.com>
2020-01-28 12:33:56 +0100

    .git ordner prüfft

---
+++ dir.js
@@ -0,0 +1,11 @@
+const isgit = require('./is-git.js')
+
+
+const direc = process.argv[2]
+
+
+
+
+isgit()
+
+
--- is-git.js
+++ is-git.js
@@ -1,6 +1,28 @@
 const path = require('path');
 const dirExists = require('directory-exists');
 
-module.exports = (relativePath) => {
+
+
+module.exports = (dir) => {
+    const b = process.argv[2]
+
+
+     dir = path.join(b,'/.git')
     // Write your code here
+    dirExists(dir, (error, result) => {
+
+        if(result == true){
+            console.log('Das ist ein Git Ordner')
+        } else {
+            console.log('Ken git Ordner')
+        }
+        console.log(result); // result is a boolean
+      });
 }
+
+
+
+
+
+
+
